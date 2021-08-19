e1cbed71b3a207647829d7c293384b3b6747607a
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-08-19 06:31:54 +0000

    Day4 completed.

---
+++ Days(1-100)/Day4/Project/roock_paper_scissor.py
@@ -0,0 +1,67 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+user_choice = int(input("Enter your choice as : \n 0 for rock \n 1 for paper \n 2 for scissor\n"))
+
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+elif user_choice == 2:
+    print(scissors)
+else:
+    print("Choice should be 0, 1 or 2")
+    exit(0)
+
+computer_choice = random.randint(0, 2)
+
+print(f"Computer choosed {computer_choice}")
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors)
+else:
+    print("Choice should be 0, 1 or 2")
+
+if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif user_choice == 0 and computer_choice == 1:
+    print("You lose!")
+elif user_choice == 1 and computer_choice == 0:
+    print("You win!")
+elif user_choice == 1 and computer_choice == 2:
+    print("You lose!")
+elif user_choice == 2 and computer_choice == 0:
+    print("You lose!")
+elif user_choice == 2 and computer_choice == 1:
+    print("You win!")
+elif user_choice == computer_choice:
+    print("It's a draw")
