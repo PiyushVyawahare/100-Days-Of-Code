168b5495f86d825664b251e3441d552562691d94
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-09-21 09:41:46 +0000

    -

---
+++ Days(1-100)/Day16/Day-16-Start.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("Coral")
+timmy.forward(100)
+timmy.backward(100)
+
+my_screen = Screen()
+print(my_screen.canvwidth)
+my_screen.exitonclick()
