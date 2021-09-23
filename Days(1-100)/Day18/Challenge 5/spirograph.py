44db8d86a0c6aa4aadfd9d82fc78fccc453f79f3
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-09-23 08:41:34 +0000

    -

---
+++ Days(1-100)/Day18/Challenge 5/spirograph.py
@@ -0,0 +1,30 @@
+import random
+import turtle
+from turtle import Turtle, Screen
+
+tim = Turtle()
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    my_tuple = (r, g, b)
+    return my_tuple
+
+
+tim.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for i in range(int(360/size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = Screen()
+screen.exitonclick()
