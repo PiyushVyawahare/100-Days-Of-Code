583f6884677ee5a46f860e36ac99f94ea168a51b
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-09-24 18:20:47 +0000

    -

---
+++ Days(1-100)/Day19/turtle_racing_game.cpp
@@ -0,0 +1,37 @@
+import random
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+new_turtle = [0, 1, 2, 3, 4, 5]
+all_turtles = []
+
+for i in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[i])
+    new_turtle.goto(x=-230, y=y_positions[i])
+    all_turtles.append(new_turtle)
+
+is_race_on = False
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner.")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner.")
+            is_race_on = False
+
+screen.exitonclick()
