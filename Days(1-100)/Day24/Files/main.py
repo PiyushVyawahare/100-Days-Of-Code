92e5ef6b9ca7a8b04740c59b9db159644da4697b
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-10-14 11:38:52 +0000

    -

--- Days(1-100)/Day24/Files/main.py
+++ Days(1-100)/Day24/Files/main.py
@@ -1,1 +1,9 @@
+with open("new_file.txt", mode="w") as file:
+    file.write("Hello everyone.")
 
+with open("my_file.txt", mode="a") as file:
+    file.write("\nHello everyone")
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
