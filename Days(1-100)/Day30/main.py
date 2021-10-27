3031d0db59e5925f4e7a40707d7b111bfdedab35
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-10-27 13:18:34 +0000

    -

--- Days(1-100)/Day30/main.py
+++ Days(1-100)/Day30/main.py
@@ -1,16 +1,25 @@
-try:
-    file = open("data.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("data.txt", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("file closed.")
-	
+# try:
+#     file = open("data.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("data.txt", "w")
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("file closed.")
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise TypeError("Height can't be greater than 3 meters.")
+
+bmi = weight / height ** 2
+
+print(bmi)
