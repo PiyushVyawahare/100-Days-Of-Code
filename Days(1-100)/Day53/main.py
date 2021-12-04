2dc10abefb150f6d8d2c7cbb42ff7d18175db869
Piyush Vyawahare <piyushvyawahare2001@gmail.com>
2021-12-04 11:25:11 +0000

    -

---
+++ Days(1-100)/Day53/main.py
@@ -0,0 +1,51 @@
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+FORM_LINK = "https://forms.gle/6eZNq6zNuZWP2B3fA"
+WEBSITE_LINK = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+
+browser_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get(WEBSITE_LINK, headers=browser_headers)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_link_elements = soup.select(".list-card-top a")
+all_links = []
+
+for link in all_link_elements:
+    href = link["href"]
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.getText().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-price")
+all_prices = [price.getText() for price in all_price_elements]
+
+CHROME_DRIVER_PATH = "D:\Desktop\Web Development\chromedriver_win32\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+for i in range(len(all_links)):
+    driver.get(FORM_LINK)
+
+    time.sleep(2)
+    address = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+
+    submit = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    address.send_keys(all_addresses[i])
+    price.send_keys(all_prices[i])
+    link.send_keys(all_links[i])
+    submit.click()
